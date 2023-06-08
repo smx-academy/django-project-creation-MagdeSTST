bf693b60be896eb2d62a80638e1f441a7033dc81
MagdeSTST <69201378+MagdeSTST@users.noreply.github.com>
2023-06-08 16:30:01 +0200

    Delete CAS2 directory

--- CAS2/Zadacha6.py
+++
@@ -1,8 +0,0 @@
-a = int(input("Внеси ја ширинат на правоаголникот:"))
-b = int(input("Внеси ја должината на правоаголникот:"))
-#perimetar 
-perimetar = 2*a+2*b 
-plostina= a*b 
-
-print(f"Периметарот на правоаголникот е {perimetar}")
-print(f"Плоштината на правоаголникот е {plostina}")
