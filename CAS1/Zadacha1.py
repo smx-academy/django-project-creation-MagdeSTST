43f64eca5bb2e253b4da99ac9a1f4d99d0a5d0bf
MagdeSTST <69201378+MagdeSTST@users.noreply.github.com>
2023-06-08 16:29:31 +0200

    Delete CAS1 directory

--- CAS1/Zadacha1.py
+++
@@ -1,5 +0,0 @@
-p_language ='Python'
-p_cas = 1
-print(f'Uchime {p_language}, denes ni e {p_cas} cas.')
-print('Uchime {}, denes ni e {} cas.'.format(p_language,p_cas) )
-print('Uchime ' + p_language +', denes ni e '+ str(p_cas) + ' cas.')
